8b82fe8411cbf7809899fda6c096cef2e561a791
Myroslav Mosiichuk <MMosiichuk@hhek.de>
2026-06-16 13:38:36 +0200

    Fighters styling update

--- src/pages/Fighters.tsx
+++ src/pages/Fighters.tsx
@@ -24,31 +24,32 @@ export default function Fighters() {
 
     return (
         <div>
-            <select
-                className="form-select form-select-sm"
-                id="category-select"
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-            >
-                <option value="Bantamweight">Bantamweight</option>
-                <option value="Catch Weight">Catch Weight</option>
-                <option value="Featherweight">Featherweight</option>
-                <option value="Flyweight">Flyweight</option>
-                <option value="Heavyweight">Heavyweight</option>
-                <option value="Light Heavyweight">Light Heavyweight</option>
-                <option value="Lightweight">Lightweight</option>
-                <option value="Middleweight">Middleweight</option>
-                <option value="Open Weight">Open Weight</option>
-                <option value="Super Heavyweight">Super Heavyweight</option>
-                <option value="Welterweight">Welterweight</option>
-                <option value="Women's Bantamweight">Women's Bantamweight</option>
-                <option value="Women's Featherweight">Women's Featherweight</option>
-                <option value="Women's Flyweight">Women's Flyweight</option>
-                <option value="Women's Lightweight">Women's Lightweight</option>
-                <option value="Women's Strawweight">Women's Strawweight</option>
-            </select>
-
-            <button onClick={handleClick}>Click me</button>
+            <div className={"selectors-container"}>
+                <select
+                    className="form-select form-select-sm"
+                    id="category-select"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                    <option value="Bantamweight">Bantamweight</option>
+                    <option value="Catch Weight">Catch Weight</option>
+                    <option value="Featherweight">Featherweight</option>
+                    <option value="Flyweight">Flyweight</option>
+                    <option value="Heavyweight">Heavyweight</option>
+                    <option value="Light Heavyweight">Light Heavyweight</option>
+                    <option value="Lightweight">Lightweight</option>
+                    <option value="Middleweight">Middleweight</option>
+                    <option value="Open Weight">Open Weight</option>
+                    <option value="Super Heavyweight">Super Heavyweight</option>
+                    <option value="Welterweight">Welterweight</option>
+                    <option value="Women's Bantamweight">Women's Bantamweight</option>
+                    <option value="Women's Featherweight">Women's Featherweight</option>
+                    <option value="Women's Flyweight">Women's Flyweight</option>
+                    <option value="Women's Lightweight">Women's Lightweight</option>
+                    <option value="Women's Strawweight">Women's Strawweight</option>
+                </select>
+                <button onClick={handleClick}>Click me</button>
+            </div>
             <div className={"d-grid gap-4 cards-container"}>
                 {apiData.map((fighter: Fighter, index: number) => (
                     <div key={index} id={"fight-" + index} className={"card"}>
